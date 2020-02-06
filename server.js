423397b0cffd034f1c55f53ff1fd7fe222eb682b
dannevernguyens <danguyenuw@gmail.com>
2020-02-05 20:01:46 -0800

    got base css set up and met requirements for feature 1 from trello board

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 'use strict';
-
+require('dotenv').config();
 const superagent = require('superagent');
 const express = require('express');
 const app = express();
@@ -14,47 +14,47 @@ app.get('/', (req, res) => {
   res.render('pages/index.ejs');
 });
 
-app.get('/book', (req, res) => {
+// app.get('/book', (req, res) => {
 
-  // select * from some database ...
-  let book = {
-    title: 'cool things',
-    author: 'awesome person'
-  };
+//   // select * from some database ...
+//   let book = {
+//     title: 'cool things',
+//     author: 'awesome person'
+//   };
 
-  res.render('pages/book', { peanuts: book })
-  // res.status(200).json(book);
-});
+//   res.render('pages/book', { peanuts: book })
+//   // res.status(200).json(book);
+// });
 
-app.get('/food', (req, res) => {
-  let foods = ['cherries', 'choclate', 'sherry'];
-  res.render('pages/food', { ingredients: foods })
-})
+// app.get('/food', (req, res) => {
+//   let foods = ['cherries', 'choclate', 'sherry'];
+//   res.render('pages/food', { ingredients: foods })
+// })
 
-app.get('/search', (req, res) => {
-  console.log(req.query);
-  res.status(200).send('You did a GET');
-});
+// app.get('/search', (req, res) => {
+//   console.log(req.query);
+//   res.status(200).send('You did a GET');
+// });
 
-app.post('/search', (req, res) => {
-  // what did the person type?
-  let title = req.body.title || "hockey";
-  // get a list from google
+// app.post('/search', (req, res) => {
+//   // what did the person type?
+//   let title = req.body.title || "hockey";
+//   // get a list from google
 
-  let url = `https://www.googleapis.com/books/v1/volumes?q=intitle:${title}`;
+//   let url = `https://www.googleapis.com/books/v1/volumes?q=intitle:${title}`;
 
-  superagent.get(url)
-    .then(data => {
-      // render the list in ejs
-      res.render('pages/books', { books: data.body.items })
-    })
-});
+//   superagent.get(url)
+//     .then(data => {
+//       // render the list in ejs
+//       res.render('pages/books', { books: data.body.items })
+//     })
+// });
 
 
-app.post('/save', (req, res) => {
-  console.log(req.query);
-  console.log(req.body);
-  res.status(200).send('You did a POST');
-});
+// app.post('/save', (req, res) => {
+//   console.log(req.query);
+//   console.log(req.body);
+//   res.status(200).send('You did a POST');
+// });
 
-app.listen(3000, () => console.log('hi'));
+app.listen(process.env.PORT, () => console.log(`up on ${process.env.PORT}`));
